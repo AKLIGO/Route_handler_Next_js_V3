0c00e96f787123ff18a01b0564e4e96e01907437
AKLIGO <koffitsejeromea@gmail.com>
2026-03-26 17:21:21 +0000

    la methode Get et Post assurer

---
+++ app/comments/data.ts
@@ -0,0 +1,7 @@
+import { text } from "stream/consumers";
+
+export const commments = [
+    {id:1, text:"This is the first comment"},
+    {id:2, text:"This is the second comment"},
+    {id:3, text:"This is the third comment"},
+]
---
+++ app/comments/route.ts
@@ -0,0 +1,18 @@
+import { headers } from "next/headers";
+import { commments } from "./data";
+export async function GET() {
+    return Response.json(commments);
+}
+
+export async function POST(request: Request) {
+    const comment = await request.json();
+    const newComment ={
+        id: commments.length + 1,
+        text: comment.text,
+    }
+    commments.push(newComment);
+    return new Response(JSON.stringify(newComment), 
+    
+{headers: {
+    "Content-Type": "application/json", },status: 201});
+}
