ed8766a177d319ecd9c95ca8d0d49191b2fec5b0
AKLIGO <koffitsejeromea@gmail.com>
2026-03-27 10:08:48 +0000

    les parametres de requetes

--- app/comments/route.ts
+++ app/comments/route.ts
@@ -1,7 +1,19 @@
 import { headers } from "next/headers";
 import { commments } from "./data";
-export async function GET() {
-    return Response.json(commments);
+import { NextRequest } from "next/server";
+
+
+// export async function GET() {
+//     return Response.json(commments);
+// }
+
+export async function GET(req: NextRequest) {
+    const searchParams = req.nextUrl.searchParams;
+    const query= searchParams.get("query");
+    const filteredComments = query 
+                 ? commments.filter(comment => comment.text.toLowerCase().includes(query.toLowerCase()))
+                 : commments;
+    return Response.json(filteredComments);
 }
 
 export async function POST(request: Request) {
