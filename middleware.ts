d2a675972fada18498e5103c70f1f6b2f1873a09
AKLIGO <koffitsejeromea@gmail.com>
2026-03-27 14:47:13 +0000

    les entetes personnaliser toujours dans middlware

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,18 @@ import { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
     // return NextResponse.redirect(new URL("/", request.url));
 
-    if(request.nextUrl.pathname === "/hello") {
-        return NextResponse.rewrite(new URL("/profile", request.nextUrl));
+    // if(request.nextUrl.pathname === "/hello") {
+    //     return NextResponse.rewrite(new URL("/profile", request.nextUrl));
+    // }
+
+    const response = NextResponse.next();
+    const themePreference = request.cookies.get("theme");
+    if (!themePreference){
+        response.cookies.set("theme", "dark");
     }
+    response.headers.set("custom-header", "custom-header-value");
+    return response;
+
 }
 
 // export const config = {
