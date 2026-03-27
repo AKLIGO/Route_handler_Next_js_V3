d3906fcf8e423e0e77799267f0697632b218221e
AKLIGO <koffitsejeromea@gmail.com>
2026-03-27 14:35:02 +0000

    middlware de redirection de route

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,14 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-    return NextResponse.redirect(new URL("/", request.url));
+    // return NextResponse.redirect(new URL("/", request.url));
+
+    if(request.nextUrl.pathname === "/hello") {
+        return NextResponse.redirect(new URL("/profile", request.nextUrl));
+    }
 }
 
-export const config = {
-    matcher: "/hello",
-};
+// export const config = {
+//     matcher: "/hello",
+// };
 
